487cf5c99c4545a658217a6a39bb70b7dc5e5021
Vinicius Isola <viniciusisola@gmail.com>
2019-01-31 14:31:12 -0500

    Rename class in Vector exercise

--- Lesson08/Exercise001_Vector/ReadUsersIntoVector.java
+++ Lesson08/Exercise001_Vector/ReadUsersIntoVector.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Vector;
 
-public class ReadUsers {
+public class ReadUsersIntoVector {
 
     public static void main (String [] args) throws IOException {
         Vector users = loadUsers(args[0]);
