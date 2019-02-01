429a06c727db1a067f35e8bb843a7fcb5e28918f
Vinicius Isola <viniciusisola@gmail.com>
2019-02-01 14:06:58 -0500

    Add snippets for Iterable and Iterator section

---
+++ Lesson08/Snippet001_Iterators_and_Iterable/IterableCSVReader.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class IterableCSVReader extends CSVReader implements Iterable {
+
+    private String[] currentRow;
+    private String [] nextRow;
+
+    public IterableCSVReader(BufferedReader reader) throws IOException {
+        super(reader);
+        currentRow = this.readRow();
+        nextRow = this.readRow();
+    } 
+
+    public Iterator iterator() {
+        return new Iterator() {
+            public boolean hasNext() {
+                return nextRow != null;
+            }
+
+            public Object next() {
+                if (nextRow == null) {
+                    throw new NoSuchElementException("No more rows to return.");
+                }
+
+                currentRow = nextRow;
+                try {
+                    nextRow = readRow();
+                } catch (IOException ioe) {
+                    throw new RuntimeException("Failed to read record.", ioe);
+                }
+                return currentRow;
+            }
+        };
+    }
+
+}
