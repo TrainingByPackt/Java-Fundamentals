d30e96dad27a39c62118498092e240bbe2cd1579
steffimonteiro <steffim@packt.com>
2019-03-04 11:19:28 +0530

    Update DeptEnum.java

--- Lesson08/Activity34/DeptEnum.java
+++ Lesson08/Activity34/DeptEnum.java
@@ -1,17 +1,17 @@
 public enum DeptEnum {
-BE(â€œBACHELOR OF ENGINEERINGâ€, 1), BCOM(â€œBACHELOR OF COMMERCEâ€, 2),
-BSC(â€œBACHELOR OF SCIENCEâ€,
-3), BARCH(â€œBACHELOR OF ARCHITECTUREâ€, 4),
-DEFAULT(â€œBACHELORâ€, 0);
-private String accronym;
+BE(“BACHELOR OF ENGINEERING”, 1), BCOM(“BACHELOR OF COMMERCE”, 2),
+BSC(“BACHELOR OF SCIENCE”,
+3), BARCH(“BACHELOR OF ARCHITECTURE”, 4),
+DEFAULT(“BACHELOR”, 0);
+private String acronym;
 private int deptNo;
-DeptEnum(String accr, int deptNo) {
-this.accronym = accr;
+DeptEnum(String acr, int deptNo) {
+this.acronym = acr;
 this.deptNo = deptNo;
 }
 
-public String getAccronym() {
-return accronym;
+public String getAcronym() {
+return acronym;
 }
 public int getDeptNo() {
 return deptNo;
@@ -19,13 +19,13 @@ return deptNo;
 
 public static void main(String[] args) {
 // Fetching the Enum using Enum name as string
-DeptEnum env = DeptEnum.valueOf(â€œBEâ€);
-System.out.println(env.getAccronym() + â€œ : â€œ + env.getDeptNo());
+DeptEnum env = DeptEnum.valueOf(“BE”);
+System.out.println(env.getAcronym() + “ : “ + env.getDeptNo());
 // Printing all the values of Enum
 for (DeptEnum e : DeptEnum.values()) {
-System.out.println(e.getAccronym() + â€œ : â€œ + e.getDeptNo()); }
+System.out.println(e.getAcronym() + “ : “ + e.getDeptNo()); }
 // Compare the two enums using the the equals() method or using //the
 == operator.
-System.out.println(DeptEnum.BE == DeptEnum.valueOf(â€œBEâ€));
+System.out.println(DeptEnum.BE == DeptEnum.valueOf(“BE”));
 }
 }
