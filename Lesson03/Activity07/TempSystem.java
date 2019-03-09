4646779509200f9b7d4727f717b8ff37256d8acc
Vinicius Isola <viniciusisola@gmail.com>
2019-03-09 09:34:42 -0500

    Fix Lesson03/Activity07/TempSystem.java

--- Lesson03/Activity07/TempSystem.java
+++ Lesson03/Activity07/TempSystem.java
@@ -1,22 +1,21 @@
-public class TempSystem
-{
-public static void main(String[] args) {
-String temp = "Low";
-String weatherWarning;
-switch (temp) { 
-case "High": 
-weatherWarning = "It’s hot outside, do not forget sunblock."; 
-break; 
-case "Low": 
-weatherWarning = "It’s cold outside, do not forget your coat."; 
-break; 
-case "Humid": 
-weatherWarning = "The weather is humid, open your windows."; 
-break;
-default: 
-  weatherWarning = "The weather looks good. Take a walk outside"; 
-  break;
-} 
-System.out.println(weatherWarning); 
-}
+public class TempSystem {
+  public static void main(String[] args) {
+    String temp = "Low";
+    String weatherWarning;
+    switch (temp) {
+    case "High":
+      weatherWarning = "It's hot outside, do not forget sunblock.";
+      break;
+    case "Low":
+      weatherWarning = "It's cold outside, do not forget your coat.";
+      break;
+    case "Humid":
+      weatherWarning = "The weather is humid, open your windows.";
+      break;
+    default:
+      weatherWarning = "The weather looks good. Take a walk outside";
+      break;
+    }
+    System.out.println(weatherWarning);
+  }
 }
