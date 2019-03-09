ec4e74a135a9aea041f233fc53c1777a3d59a79d
Vinicius Isola <viniciusisola@gmail.com>
2019-03-09 09:33:45 -0500

    Fix Lesson03/Exercise07/DeliveryFee.java

--- Lesson03/Exercise07/DeliveryFee.java
+++ Lesson03/Exercise07/DeliveryFee.java
@@ -1,21 +1,18 @@
-public class DeliveryFee{
-public static void main(String[] args){
-int distance = 10;
-int fee = 0;
-if (distance > 0 && distance < 5){
-fee = 2;
-else if (distance >= 5 && distance < 15){
-fee = 5;
-}
-else if (distance >= 15 && distance < 25){
-fee = 10;
-}else if (distance >= 25 && distance < 50){
-fee = 15;
-}
-else {
-fee = 20;
-}
-System.out.println("Delivery Fee: " + fee);
-}
-}
+public class DeliveryFee {
+    public static void main(String[] args) {
+        int distance = 10;
+        int fee = 0;
+        if (distance > 0 && distance < 5) {
+            fee = 2;
+        } else if (distance >= 5 && distance < 15) {
+            fee = 5;
+        } else if (distance >= 15 && distance < 25) {
+            fee = 10;
+        } else if (distance >= 25 && distance < 50) {
+            fee = 15;
+        } else {
+            fee = 20;
+        }
+        System.out.println("Delivery Fee: " + fee);
+    }
 }
