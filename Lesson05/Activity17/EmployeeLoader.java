e0adbdfe476f7bbe56b51df1ba79c9a17d810d18
Vinicius Isola <viniciusisola@gmail.com>
2019-03-09 09:30:40 -0500

    Fix Lesson05/Activity17/EmployeeLoader.java

--- Lesson05/Activity17/EmployeeLoader.java
+++ Lesson05/Activity17/EmployeeLoader.java
@@ -1,20 +1,23 @@
 import java.util.Random;
+
 public class EmployeeLoader {
-private static Random random = new Random(15);
-public static Employee getEmployee() {
+    private static Random random = new Random(15);
+
+    public static Employee getEmployee() {
         int nextNumber = random.nextInt(4);
-        switch(nextNumber) {
-            case 0:
-                // A sales person with total sales between 5000 and 1550000
-                double grossSales = random.nextDouble() * 150000 + 5000;
-                return new SalesWithCommission(grossSales);
-            case 1:
-                return new Manager();
-            case 2:
-                return new Engineer();
-            case 3:
-                return new Sales();
-            default:
-                return new Manager();
+        switch (nextNumber) {
+        case 0:
+            // A sales person with total sales between 5000 and 1550000
+            double grossSales = random.nextDouble() * 150000 + 5000;
+            return new SalesWithCommission(grossSales);
+        case 1:
+            return new Manager();
+        case 2:
+            return new Engineer();
+        case 3:
+            return new Sales();
+        default:
+            return new Manager();
         }
     }
+}
