5ed872288e3b8c882a3d932816c658f8dc24e982
Vinicius Isola <viniciusisola@gmail.com>
2019-03-09 09:31:58 -0500

    Fix Lesson04/Exercise11/Person.java

--- Lesson04/Exercise11/Person.java
+++ Lesson04/Exercise11/Person.java
@@ -1,28 +1,32 @@
 public class Person {
-//Properties
-int age;
-int height;
-String name;
-//Methods
-public void walk(){
-//Do walking operations here
-System.out.println("Walking...");
-}
-public void sleep(){
-//Do sleeping operations here
-System.out.println("Sleeping...");
-}
-private void takeShower(){
-//Do take shower operations here
-System.out.println("Taking a shower...");
-}
-public void walk(int speed){
-//Do walking operations here
-if (speed > 10)
-{
-System.out.println("Walking...");
-}
-Person me = new Person();
-Person myNeighbour = new Person();
-Person lecturer = new Person();
+    // Properties
+    int age;
+    int height;
+    String name;
+
+    // Methods
+    public void walk() {
+        // Do walking operations here
+        System.out.println("Walking...");
+    }
+
+    public void sleep() {
+        // Do sleeping operations here
+        System.out.println("Sleeping...");
+    }
+
+    private void takeShower() {
+        // Do take shower operations here
+        System.out.println("Taking a shower...");
+    }
+
+    public void walk(int speed) {
+        // Do walking operations here
+        if (speed > 10) {
+            System.out.println("Walking...");
+        }
+        Person me = new Person();
+        Person myNeighbour = new Person();
+        Person lecturer = new Person();
+    }
 }
