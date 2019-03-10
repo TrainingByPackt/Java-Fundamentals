68faaaff6bbebbd40bdcbe67646afe9b28fa98cc
Vinicius Isola <viniciusisola@gmail.com>
2019-03-09 21:02:17 -0500

    Format Lesson05/Activity19/HospitalTest.java

--- Lesson05/Activity19/HospitalTest.java
+++ Lesson05/Activity19/HospitalTest.java
@@ -1,11 +1,11 @@
 public class HospitalTest {
-public static void main(String[] args){
-Doctor doctor = new Doctor();
-People people = new People();
-String str = doctor.getPersonType();
-String str1 = patient.getPersonType();
-System.out.println(str);
-System.out.println(str1);
+   public static void main(String[] args) {
+      Doctor doctor = new Doctor();
+      People patient = new People();
+      String str = doctor.getPersonType();
+      String str1 = patient.getPersonType();
+      System.out.println(str);
+      System.out.println(str1);
 
    }
 }
