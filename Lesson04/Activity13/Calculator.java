195b3b688b9a6b911e8afcdd1d8cd727bac8f9bd
Vinicius Isola <viniciusisola@gmail.com>
2019-03-10 14:10:46 -0400

    Add sample output for Activity13

--- Lesson04/Activity13/Calculator.java
+++ Lesson04/Activity13/Calculator.java
@@ -1,26 +1,28 @@
 public class Calculator {
-private final double operand1;
-private final double operand2;
-private final String operator;
+    private final double operand1;
+    private final double operand2;
+    private final String operator;
 
-public Calculator(double operand1, double operand2, String operator){
-this.operand1 = operand1;
-this.operand2 = operand2;
-this.operator = operator;
-}
-public double operate() {
-if (this.operator.equals("-")) {
-return operand1 - operand2;
-} else if (this.operator.equals("x")) {
-return operand1 * operand2;
-} else if (this.operator.equals("/")) {
-return operand1 / operand2;
-} else {
-// If operator is sum or unknown, return sum
-return operand1 + operand2;
-}
-}
-public static void main (String [] args) {
+    public Calculator(double operand1, double operand2, String operator) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+        this.operator = operator;
+    }
+
+    public double operate() {
+        if (this.operator.equals("-")) {
+            return operand1 - operand2;
+        } else if (this.operator.equals("x")) {
+            return operand1 * operand2;
+        } else if (this.operator.equals("/")) {
+            return operand1 / operand2;
+        } else {
+            // If operator is sum or unknown, return sum
+            return operand1 + operand2;
+        }
+    }
+
+    public static void main(String[] args) {
         System.out.println("1 + 1 = " + new Calculator(1, 1, "+").operate());
         System.out.println("4 - 2 = " + new Calculator(4, 2, "-").operate());
         System.out.println("1 x 2 = " + new Calculator(1, 2, "x").operate());
