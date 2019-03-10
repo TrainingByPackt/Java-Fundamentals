6d92c51dafca06e7ff182d0c50d68786f2a9cf8a
Vinicius Isola <viniciusisola@gmail.com>
2019-03-10 15:23:37 -0400

    Add executions for Exercise30

--- Lesson07/Exercise30/EmailSender.java
+++ Lesson07/Exercise30/EmailSender.java
@@ -5,7 +5,7 @@ public class EmailSender {
     public static void main (String [] args) throws Exception {
         System.out.printf("Sending email to %s...\n", args[0]);
         // Let's take some time to send the email
-        Thread.sleep(new Random().nextInt(1000));
+        Thread.sleep(new Random().nextInt(100));
         System.out.printf("Email sent to %s!\n", args[0]);
     }
 
