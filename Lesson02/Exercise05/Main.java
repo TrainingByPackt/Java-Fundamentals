28cf212a64e5f50844040d1c7fa79c2d35111309
Vinicius Isola <viniciusisola@gmail.com>
2019-05-14 22:18:37 -0400

    Reformat Lesson 1 and 2

--- Lesson02/Exercise05/Main.java
+++ Lesson02/Exercise05/Main.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
-public class Main
-{
-static Scanner sc = new Scanner(System.in);
-public static void main(String[] args)
-{
-System.out.println("Enter a Number: ");
-int num1 = sc.nextInt();
-System.out.println("Entered value is: " + num1);
-float fl1 = num1;
-System.out.print("Entered value as a floating point variable is: " +
-fl1);
-}
+
+public class Main {
+  static Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    System.out.println("Enter a Number: ");
+    int num1 = sc.nextInt();
+    System.out.println("Entered value is: " + num1);
+    float fl1 = num1;
+    System.out.print("Entered value as a floating point variable is: " + fl1);
+  }
 }
