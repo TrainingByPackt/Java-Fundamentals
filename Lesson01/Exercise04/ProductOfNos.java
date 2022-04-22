44a50c18d0a041ddf077f51ad251d01b83a27c1f
Gary Chike <chikega@pm.me>
2022-04-22 13:39:25 -0400

    Update ProductOfNos.java
    added 'throws IOException' which is in the published book.

--- Lesson01/Exercise04/ProductOfNos.java
+++ Lesson01/Exercise04/ProductOfNos.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class ProductOfNos {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     System.out.println("Enter the first number");
     int var1 = Integer.parseInt(System.console().readLine());
     System.out.println("Enter the Second number");
