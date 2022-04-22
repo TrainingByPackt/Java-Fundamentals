0be185cc3d64ec3e917c8d331578edc0a7ece154
Vinicius Isola <viniciusisola@gmail.com>
2022-04-22 15:11:24 -0400

    Remove unecessary throws declaration and import

--- Lesson01/Exercise04/ProductOfNos.java
+++ Lesson01/Exercise04/ProductOfNos.java
@@ -1,8 +1,6 @@
-import java.io.IOException;
-
 public class ProductOfNos {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     System.out.println("Enter the first number");
     int var1 = Integer.parseInt(System.console().readLine());
     System.out.println("Enter the Second number");
